6b8cf1e4cf5f7eee1603e87ce08f0c77f36714b2
ahillo <oharjola@gmail.com>
2013-12-13 16:59:23 +0200

    Initial commit, added files.

---
+++ source/NodeGraphControl/Globals.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NodeGraphControl
+{
+    public static class Globals
+    {
+        public static Dictionary<string, NodeGraphDataType> KnownDataTypes
+        {
+            get;
+            set;
+        }
+    }
+}
